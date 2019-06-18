d4c49bf5a102f8e8a2f4068704b82d528c058208
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-06-18 22:55:11 +0200

    first

---
+++ src/Pallete.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Pallete extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pallete</h1>
+            </div>
+        )
+    }
+}
+
+export default Pallete;
